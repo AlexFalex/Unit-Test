2ccc4fa0c4f4c11ad543f0694dcdf06f35df233e
Alex Nikonov <KarlosRPG@yandex.ru>
2024-11-02 20:47:51 +0300

    Initial commit

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,25 @@
+package ru.netology;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+        NewMathOperations myMathOperations = new NewMathOperations();
+        List<Integer> list = Arrays.asList(12, 77, 32, 71, -54, -12, 12, -17, 45);
+        System.out.println("Операции с листом: " + list);
+        System.out.println("Лист положительных чисел: " + myMathOperations.positiveNumbers(list));
+        System.out.println("Лист четных чисел: " + myMathOperations.evenNumber(list));
+        System.out.println();
+        System.out.println("Расчет комиссии:");
+        int sum1 = 750;
+        System.out.println("С суммы " + sum1 + " руб., комиссия составит: " +
+                +myMathOperations.commissionPercentCalc(sum1) + " руб.");
+
+        int sum2 = 75_700;
+        System.out.println("С суммы " + sum2 + " руб., комиссия составит: " +
+                +myMathOperations.commissionPercentCalc(sum2) + " руб.");
+
+    }
+}
